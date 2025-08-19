684d17d06c61d1d59c18e8bdb6f8b093632b05e6
Michal Olesiak <micholesiak@gmail.com>
2025-08-19 17:26:56 +0200

    feat: remove Cypress tests and related files in preparation for Playwright migration

--- cypress/support/viewports.ts
+++
@@ -1,7 +0,0 @@
-export const VIEWPORTS = [
-  { name: 'mobile', width: 375, height: 667 },
-  { name: 'tablet', width: 768, height: 1024 },
-  { name: 'desktop', width: 1280, height: 720 }
-];
-
-export default VIEWPORTS;
